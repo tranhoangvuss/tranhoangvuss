71e2f17d31a6e9379ab7ea88af7376ad10ee7c5f
Rainny <tranhoangvuc@gmail.com>
2024-07-30 07:52:46 +0700

    a

--- src/App.js
+++ src/App.js
@@ -2,23 +2,47 @@ import logo from './logo.svg';
 import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
-const App = () => {
-  const count = useSelector(state => state.counter.count);
-  const dispatch = useDispatch();
+import MyComponent from './components/MyComponent';
+import React from 'react';
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world!
-        </p>
-        <div>Count = {count}</div>
-        <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-        <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    name: 'a',
+    age: '19'
+  }
+
+  render() {
+    return (
+      <div >
+
+        Hello world!
+        my name is {this.state.name}
+      </div>
+    )
+  }
 }
 
+
+
+
+
+// const App = () => {
+//   const count = useSelector(state => state.counter.count);
+//   const dispatch = useDispatch();
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Hello world!
+
+//         </p>
+//         <div>Count = {count}</div>
+//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
+//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+//       </header>
+//     </div>
+//   );
+// }
 export default App;
---
+++ src/components/MyComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+class MyComponent extends React.Component {
+    render() {
+        return (
+            <div>
+                My first Component
+
+            </div>
+        )
+    }
+}
+export default MyComponent;
